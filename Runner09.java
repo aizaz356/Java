34a13f28420f795410aaf1b1a06b5a6ce669bf81
aizaz356 <aizaz3448@gmail.com>
2026-06-06 23:09:41 +0500

    Add changes in  Runner09.java

--- Runner09.java
+++ Runner09.java
@@ -1,37 +1,40 @@
 interface Plugin {
     // serves as a contract
-    public void draw();
-    public void save();
-
+    public  void draw();
+    public  void save();
 }
 
 
 class TextEditorPlugin implements Plugin {
-    // public void draw() {
-    //    System.out.println("Drawing text");
-    //}
+        @Override
+        public void draw() {
+        System.out.println("Drawing text");
+    }
 
-    //public void save() {
-    //    System.out.println("Saving text");
-    //}
+        @Override
+        public void save() {
+        System.out.println("Saved text");
+    }
 }
 
-// class PDFviewerPlugin implements Plugin {
-//     public void draw() {
-//         System.out.println("Showing PDF");
-//     }
-//
-//     public void save() {
-//         System.out.println("Saved PDF annotations");
-//     }
-// }
+//class PDFViewerPlugin implements Plugin {                 //  if we initialize one class the other class and its functions are comments //
+        //@Override
+        //public void draw() {
+        //System.out.println("Showing PDF");
+    //}
+
+        //@Override
+        //public void save() {                           
+        //System.out.println("Saved PDF annotations");
+    //}
+//}
 
 public class Runner09 {
     public static void main(String args[]) {
         // assume this is your atom editor
-
-        Plugin p = new TextEditorPlugin();    // reference variable of interference can hold implementing class object
-        // plugin p = new PDFViewerPlugin();
+                                                        
+        TextEditorPlugin p = new TextEditorPlugin();    // reference variable of interference can hold implementing class object
+        //PDFViewerPlugin p = new PDFViewerPlugin();   
 
         p.draw();
         p.save();
