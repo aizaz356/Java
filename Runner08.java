53a145ad7d19fc2c46b78cde2f04f69cb72b591d
aizaz356 <aizaz3448@gmail.com>
2026-06-06 17:03:42 +0500

    add comments

--- Runner08.java
+++ Runner08.java
@@ -1,4 +1,4 @@
-class Polygon {
+abstract class Polygon {
     int numSides;
 
     public Polygon(int numSides) {
@@ -6,9 +6,9 @@ class Polygon {
     }
 
     // 01: add abstract, then make class abstract then don't instantiate
-    public double getArea() {
-        return 2.0;     // no idea how to calculate this!
-    }
+    public abstract double getArea(); // {
+        //return 2.0;
+    //}
 }
 
 class Rectangle extends Polygon {
@@ -33,13 +33,13 @@ class Rectangle extends Polygon {
 
 public class Runner08 {
     public static void main(String args[]) {
-        Polygon p = new Polygon(3);
-        System.out.println("Area of poly: " + p.getArea());  // this does not make sense
+        //Polygon p = new Polygon(3);  // cannot instantiate abstract class
+        //System.out.println("Area of poly: " + p.getArea());  // this does not make sense .. make it
 
-        Rectangle r = new Rectangle();
-        // Polygon r = new Rectangle();    // 02 -- parent class reference var, child object
+        //Rectangle r = new Rectangle();
+        Polygon r = new Rectangle();    // 02 -- parent class reference var(variable), child object   // In this,a Parent class reference variable can access/refer to the child instance(object) this is called "polymorphism"  but it can used  only the methods that are declared in the parent class. It cannot used the methods that are specific to the child class.
 
-        r.setSides(4, 4);
+        //r.setSides(4, 4);
         System.out.println("Area of rect: " + r.getArea());
     }
 }
