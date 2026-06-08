a0965beb7afc8676755b19c0d139ee88c6316869
aizaz356 <aizaz3448@gmail.com>
2026-06-08 15:33:16 +0500

    Add Changes

--- Runner11.java
+++ Runner11.java
@@ -13,28 +13,27 @@ public class Runner11 {
         vi.add(4);
         vi.add(5);
         vi.add("something");
+    
+       printVector(vi);  // print the contents of the vector
+       System.out.println(vi);    // can also output directly
+   }
 
-        printVector(vi);  // print the contents of the vector
-        // System.out.println(vi);    // can also output directly
+   //public void stringVectorTest() {
+      //Vector<String> vs = new Vector<String>();   // specify type of vector
+      //vs.add("first");
+      //vs.add("second");
+      //vs.add("third");  
+      //
+      //System.out.println(vs);  // print the contents of the vector
+   //}
 
-    }
-
-//   public void stringVectorTest() {
-//        Vector<String> vs = new Vector<String>();   // specify type of vector
-//        vs.add("first");
-//        vs.add("second");
-//        vs.add(third);  
-//
-//        System.out.println(vs);  // print the contents of the vector
-//
-//}
 
 
 public static void main(String args[]) {
         Runner11 r = new Runner11();
         r.intVectorTest();  // test the int vector
 
-        // r.stringVectorTest();  // test the string vector
+        //r.stringVectorTest();  // test the string vector
 
         // comment out intvectorTest definition to get rid of warning
     }
